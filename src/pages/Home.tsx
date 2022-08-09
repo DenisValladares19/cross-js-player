ca807fbb69dbf68e12d68bc35f0d6d44072f4ea8
Edgardo Palacios <edgard07hp@gmail.com>
2022-08-08 21:15:18 -0600

    ✨ Creating a song card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,34 @@
 import { motion } from 'framer-motion'
 
+import SongCard from '@components/SongCard'
+
+interface ISong {
+    imgSong: string
+    titleSong: string
+    artistSong: string
+    durationSong: string
+    onClick: (song: any) => void
+}
+
+const Songs: ISong[] = [
+    {
+        imgSong:
+            'https://thewichitan.com/wp-content/uploads/2022/05/Bad-Bunny-Un-Verano-Sin-Ti-1.jpeg',
+        titleSong: 'Song 1',
+        artistSong: 'Artist 1',
+        durationSong: '3:00',
+        onClick: () => {},
+    },
+    {
+        imgSong:
+            'https://thewichitan.com/wp-content/uploads/2022/05/Bad-Bunny-Un-Verano-Sin-Ti-1.jpeg',
+        titleSong: 'Song 2',
+        artistSong: 'Artist 2',
+        durationSong: '3:00',
+        onClick: () => {},
+    },
+]
+
 const Home = () => {
     return (
         <motion.div
@@ -8,7 +37,7 @@ const Home = () => {
             exit={{ x: -100, opacity: 0 }}
             transition={{ duration: 1, type: 'spring' }}
         >
-            Home
+            <SongCard songs={Songs} />
         </motion.div>
     )
 }
