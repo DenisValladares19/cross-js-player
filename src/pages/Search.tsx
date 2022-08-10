b51b457d74e34225c626efed1c9ced6941bad50e
Denis Valladares <denisvalladares402@gmail.com>
2022-08-09 22:53:23 -0600

    integration (InfinityScroll, Context, dispatch and state)

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,26 +1,31 @@
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
-import { ResultSearch, Item } from '@root/interfaces/ResultSearch'
+import { Item, ResultSearch } from '@root/interfaces/ResultSearch'
+import { truncate } from '@helpers/string-utils'
+import { TypeActionSearch } from '@root/interfaces/StateSearch'
 
 // components and hooks
 import Avatar from '@components/Avatar'
 import Input from '@components/Input'
 import useApi from '@hooks/useApi'
+import EmptySearchImage from '@components/EmptySearchImage'
+import CardDisplay from '@components/CardDisplay'
+import useWindowSize from '@hooks/useWindowSize'
+import useMusicContext from '@hooks/useMusicContext'
+import InfinityScroll from '@components/InfinityScroll'
 
 // assets
 import backIcon from '@assets/icon/back.svg'
-import EmptySearchImage from '@components/EmptySearchImage'
-import CardDisplay from '@components/CardDisplay'
-import { truncate } from '@helpers/string-utils'
-import useWindowSize from '@hooks/useWindowSize'
+import { deleteRepeteInArray } from '@helpers/array-utils'
 
 const Search = () => {
-    const [search, setSearch] = useState<string>('')
     const { data, fetchData } = useApi<ResultSearch>()
-    const [results, setResults] = useState<Item[]>([])
     const [delayTimer, setDelayTimer] = useState<NodeJS.Timeout>()
     const { width: widthWindow } = useWindowSize()
+    const { search, dispatchSearch } = useMusicContext()
+    const [isLoadMore, setIsLoadMore] = useState<boolean>(false)
+    const [nextPage, setNextPage] = useState<string>('')
 
     useEffect(() => {
         return () => {
@@ -31,18 +36,48 @@ const Search = () => {
     useEffect(() => {
         if (!data.isLoading && data.isSuccess) {
             if (data.data && data.data.items.length > 0) {
-                // let newResult = [...results, ...data.data?.items]
-                // setResults(deleteRepeteInArray(newResult, 'id', 'videoId'))
-                setResults([...data.data.items])
+                setNextPage(data.data.nextPageToken)
+                if (isLoadMore && search.listItems) {
+                    const temp = [...search.listItems, ...data.data.items]
+                    dispatchSearch({
+                        type: TypeActionSearch.SET_LIST_ITEMS,
+                        payload: {
+                            listItems: deleteRepeteInArray(
+                                temp,
+                                'id',
+                                'videoId'
+                            ),
+                        },
+                    })
+                } else {
+                    dispatchSearch({
+                        type: TypeActionSearch.SET_LIST_ITEMS,
+                        payload: { listItems: data.data.items },
+                    })
+                }
             }
         } else if (!data.isLoading && data.isError) {
-            setResults([])
+            dispatchSearch({
+                type: TypeActionSearch.SET_LIST_ITEMS,
+                payload: { listItems: [] },
+            })
         }
     }, [data])
 
     const handleSearch = (value: string) => {
+        setIsLoadMore(false)
         clearTimeout(delayTimer)
-        setSearch(value)
+        dispatchSearch({
+            type: TypeActionSearch.CHANGE_SEARCH,
+            payload: { search: value },
+        })
+
+        if (!value || value === '') {
+            dispatchSearch({
+                type: TypeActionSearch.SET_LIST_ITEMS,
+                payload: { listItems: [] },
+            })
+        }
         setDelayTimer(
             setTimeout(() => {
                 fetchData(`search?q=${value}`)
@@ -50,12 +85,30 @@ const Search = () => {
         )
     }
 
+    const handleNextPage = () => {
+        setIsLoadMore(true)
+        fetchData(`search?q=${search.search}&nextPageToken=${nextPage}`)
+    }
+
     const getSizetitle = (width: number | undefined): number => {
         if (!width) {
             return innerWidth / 12
         }
-
         return width / 12
+    }
+
+    const getHasError = (): boolean => {
+        if (!search.listItems) {
+            return false
+        }
+
+        if (!data.data?.pageInfo.totalResults) {
+            return false
+        }
+        const { totalResults, resultsPerPage } = data.data.pageInfo
+        const total = search.listItems.length + resultsPerPage
+
+        return total < totalResults
     }
 
     return (
@@ -72,30 +125,36 @@ const Search = () => {
                 <div className="input-search">
                     <Input
                         placeHolder="Buscar canción, artista..."
-                        value={search || ''}
+                        value={search.search || ''}
                         setValue={handleSearch}
-                        loading={data?.isLoading}
+                        loading={!isLoadMore && data?.isLoading}
                     />
                 </div>
                 <Avatar />
             </div>
-
-            {results.length > 0
-                ? results.map((item: Item) => (
-                      <CardDisplay
-                          author={item.snippet.channelTitle}
-                          title={truncate(
-                              item.snippet.title,
-                              getSizetitle(widthWindow)
-                          )}
-                          imageSrc={item.snippet.thumbnails.medium.url}
-                          videoId={item.id.videoId}
-                          key={item.id.videoId}
-                      />
-                  ))
-                : null}
-
-            {!results || results.length === 0 ? (
+            <InfinityScroll
+                className="container-cards"
+                hasMore={getHasError()}
+                fetchMore={handleNextPage}
+                isLoading={isLoadMore && data.isLoading}
+                setValue={setIsLoadMore}
+            >
+                {search.listItems && search.listItems.length > 0
+                    ? search.listItems.map((item: Item) => (
+                          <CardDisplay
+                              author={item.snippet.channelTitle}
+                              title={truncate(
+                                  item.snippet.title,
+                                  getSizetitle(widthWindow)
+                              )}
+                              imageSrc={item.snippet.thumbnails.medium.url}
+                              videoId={item.id.videoId}
+                              key={item.id.videoId}
+                          />
+                      ))
+                    : null}
+            </InfinityScroll>
+            {!search.listItems || search.listItems.length === 0 ? (
                 <div className="container-empty">
                     <EmptySearchImage />
                     <div className="label-empty">
@@ -108,6 +167,19 @@ const Search = () => {
 }
 
 const Wrapper = styled(motion.div)`
+    height: calc(100vh - 60px);
+
+    .container-cards {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: calc(100% - 74px - 60px);
+        overflow-y: auto;
+        &::-webkit-scrollbar {
+            display: none;
+        }
+    }
+
     .container-search {
         display: flex;
         justify-content: space-between;
